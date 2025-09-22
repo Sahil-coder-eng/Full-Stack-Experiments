2efbe9c8d83118b23fe725dd5aa6c81f1d897f82
Sahil Samal <23BCS12495@cuchd.in>
2025-09-22 10:21:24 +0530

    Update ProductCard.jsx

--- ProductCard.jsx
+++ ProductCard.jsx
@@ -1,18 +1,19 @@
+// src/components/ProCard.jsx
 import React from "react";
 
-// Reusable ProductCard component
-const ProductCard = ({ name, price, inStock }) => {
+const ProductCard = ({ name, price, stock }) => {
   return (
     <div style={{
       border: "1px solid #ccc",
       borderRadius: "8px",
       padding: "16px",
-      width: "250px",
-      margin: "10px"
+      margin: "8px",
+      width: "200px",
+      boxShadow: "2px 2px 12px rgba(0,0,0,0.1)"
     }}>
       <h2>{name}</h2>
       <p>Price: ${price}</p>
-      <p>Status: {inStock ? "In Stock" : "Out of Stock"}</p>
+      <p>Status: {stock ? "In Stock" : "Out of Stock"}</p>
     </div>
   );
 };
